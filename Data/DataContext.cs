2100b9d41bd957d7043ee8b08e6b3e4f3e3b2a09
Stéphane <s.bressani@bluewin.ch>
2019-01-04 19:57:53 +0100

    Sauvegarde udemy cours en ligne

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace PartagesWeb.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<Categorie> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
