efb6089af68c3d5656f3e45a28f7435f4b7a612f
stephaneworkspace <s.bressani@bluewin.ch>
2019-02-07 17:36:13 +0100

    ajout de 2 models

---
+++ Models/Section.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartagesWeb.API.Models
+{
+    public class Section
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Icone { get; set; }
+        public string Type { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ Models/TitreMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartagesWeb.API.Models
+{
+    public class TitreMenu
+    {
+        public int Id { get; set; }
+        // S'inspirer de vega et voir si c'est possible ? la possibilité que ça soit vide
+        public Section Section { get; set; }
+        public int SectionId { get; set; }
+        public string Nom { get; set; }
+        public int Position { get; set; }
+    }
+}
