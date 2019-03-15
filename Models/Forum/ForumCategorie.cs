021ab0cb75bfc5a54b6daa3a69381897af74370e
stephaneworkspace <s.bressani@bluewin.ch>
2019-03-15 15:58:00 +0100

    backup

---
+++ Models/Forum/ForumCategorie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartagesWeb.API.Models.Forum
+{
+    public class ForumCategorie
+    {
+        /// <summary>
+        /// Clé principale
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nom du topic
+        /// </summary>
+        public string Nom { get; set; }
+    }
+}
---
+++ Models/Forum/ForumTopic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartagesWeb.API.Models.Forum
+{
+    /// <summary>
+    /// Topic du forum
+    /// </summary>
+    public class ForumTopic
+    {
+        /// <summary>
+        /// Clé principale
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Clé principal de la catégorie
+        /// </summary>
+        public int ForumCategorieId { get; set; }
+        /// <summary>
+        /// Relation avec model User
+        /// </summary>
+        public virtual ForumCategorie ForumCategorie { get; set; }
+        /// <summary>
+        /// Clé principal de l'utilisateur
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// Relation avec model User
+        /// </summary>
+        public virtual User User { get; set; }
+        /// <summary>
+        /// Nom du topic
+        /// </summary>
+        public string Nom { get; set; }
+        /// <summary>
+        /// Date du topic
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Nombre de view
+        /// </summary>
+        public int View { get; set; }
+    }
+}
